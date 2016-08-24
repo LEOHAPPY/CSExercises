67b5c072494ff9275a549291e47c880e4856cbb7
leo <leo@LEO8F3F>
2016-08-24 22:27:28 +0800

    More Concise

--- CSExercises/SectionG/ExG3.cs
+++ CSExercises/SectionG/ExG3.cs
@@ -1,54 +1,80 @@
-﻿//using System;
+﻿using System;
 
-//namespace CSExercises
-//{
-//    //The C# language gives facilities for changing cases all to upper or lower - 
-//    //however, not to title case.  Write a program that would convert a given sentence/phrase to title case.
-//    //Example
-//    //“institute of systems science”   =>   “Institute Of System Science”
+namespace CSExercises
+{
+    //The C# language gives facilities for changing cases all to upper or lower - 
+    //however, not to title case.  Write a program that would convert a given sentence/phrase to title case.
+    //Example
+    //“institute of systems science”   =>   “Institute Of System Science”
 
-//    public class ExG3
-//    {
-//        public static void Main(string[] args)
-//        {
-//            Console.Write("Please enter a phrase: ");
-//            string phrase = Console.ReadLine();
+        //Assume: input only characters and with space at the end 
 
-//            string newphrase = ToTitleCase(phrase);
+    public class ExG3
+    {
+        public static void Main(string[] args)
+        {
 
-//            Console.WriteLine(newphrase);
-//        }
+            Console.Write("Please enter a phrase: ");
+            string phrase = Console.ReadLine();
 
-//        public static string ToTitleCase(string phrase)
-//        {
-//            //YOUR CODE HERE
-//            string result;
-//            string[] phraseSplit = phrase.Split(' ');
-//            string[] toTitle = new string[phraseSplit.Length];
+            DateTime oTimeBegin = DateTime.Now;
 
-//            for (int i = 0; i < phraseSplit.Length; i++) 
-//            {
-//                for (int j = 0; j < phraseSplit[j].Length; j++)
-//                {
-//                    if (j == 0)
-//                    {
-//                        toTitle[j] = phraseSplit[i-32];
-//                    }
-//                    else
-//                        toTitle[j] = phraseSplit[j];
-//                }
-//            }
+            Console.WriteLine(ToTitleCase(phrase));
+            //Console.WriteLine(ToTitleCase2(phrase));
+            // record time // just wanna to test speed 
+            DateTime oTimeEnd = DateTime.Now;
+            TimeSpan oTime = oTimeEnd.Subtract(oTimeBegin);
+            Console.WriteLine(oTime.ToString());
+        }
+
+        public static char[] ToTitleCase(string phrase)
+        {
+            //YOUR CODE HERE
+            //Pretreatment 
+            //phrase = phrase.ToLower();
+            phrase = phrase.Trim();
+
+            //become to chatArray
+            char[] phraseChar = phrase.ToCharArray();
+
+            //lower is more 32 than upper in ASICAA
+            phraseChar[0] = (char)(phraseChar[0] - 32);
+
+            for (int i = 0; i < phraseChar.Length; i++) 
+            {
+                if (phraseChar[i]==' ')
+                {
+                    phraseChar[i + 1] = (char)(phraseChar[i + 1] - 32);
+                }
+            }
+            return phraseChar;
+        }
+
+        public static string ToTitleCase2(string phrase)
+        {
+            string[] phraseSplit = phrase.Split(' ');
+
+            string b = "";
+            for (int i = 0; i < phraseSplit.Length; i++) 
+            {
+                string a = "";
+
+                for (int j = 0; j < phraseSplit[i].Length; j++) 
+                {
+                    a = a + phraseSplit[i][j];
+
+                    if (j ==0)
+                    {
+                        a = a.ToUpper();
+                    }
+                }
+                b = b + a + " ";
+
+            }
+
+            return b;
+        }
 
 
-
-
-
-
-
-
-//            return result;
-
-
-//        }
-//    }
-//}
+    }
+}
--- CSExercises/SectionH/ExH3.cs
+++ CSExercises/SectionH/ExH3.cs
@@ -1,20 +1,53 @@
-﻿namespace CSExercises
+﻿using System;
+
+namespace CSExercises
 {
     public class ExH3
     {
-        //Write a function (static method) that would take in an integer and 
-        //return the hexadecimal.  Print out the Hex of all numbers 1 to 100 and 
+        //Write a function (static method) that would take in an integer and return the hexadecimal.  
+
+        //Print out the Hex of all numbers 1 to 100 and 
+
         //compare your answer with that of the built in function.
-        public static string Hex(int i)
+
+
+       static char[] rule = new char[] { 'A', 'B', 'C', 'D', 'E', 'F' };
+
+       public static void Main()
+        {
+            Console.WriteLine("please input an integer");
+            int input = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(printHex());
+            Console.WriteLine(Hex(input));
+        }
+
+        public static string Hex(int x)
         {
             //YOUR CODE HERE
-            return null;
+            string hex = "";
+            string hexSplit = "";
 
+            int remainder = 0;
 
+            do
+            {
+                remainder = x % 16;
+                if (remainder < 10)
+                {
+                    hexSplit = remainder.ToString();
+                }
+                else if (remainder >= 10)
+                {
+                    hexSplit = Convert.ToString(rule[remainder - 10]);
+                }
+                hex = hexSplit + hex;
+                x = (x - remainder) / 16;
+            } while (x >= 16);
 
-
-
-
+            hex = x + hex;
+           
+            return hex;
         }
+        
     }
 }
